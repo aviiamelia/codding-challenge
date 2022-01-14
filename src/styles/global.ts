56ea96b01c88420d7c5f2557eb788b4047871129
Rafael Leonardo <rafael.zoe26@gmail.com>
2022-01-14 07:09:20 -0300

    feat: global styles added

---
+++ src/styles/global.ts
@@ -0,0 +1,25 @@
+import {createGlobalStyle} from "styled-components"
+
+export default createGlobalStyle`
+     *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        outline: none;
+    }
+    :root{
+        --purple:#512F82,
+        --acquaGreen:#37CCBD,
+        --darkAcquaGreen:#29978C,
+        --white:#D9FFFD,
+        --darkGrey:#2D3333
+    }
+    body {
+        font-family: 'Roboto Mono', monospace;
+    }
+    button{
+        cursor: pointer;
+        border:none;
+        border-radius: 8px;
+    }
+`
